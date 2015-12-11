e7ab6d0972f1d719424150870ca0f2c3f85bde27
bit-user <bit-user@bit>
2015-12-11 15:28:11 +0900

    12.11 guestbook3 - Spring MVC pattern

--- src/main/java/com/hanains/guestbook/controller/GuestBookController.java
+++ src/main/java/com/hanains/guestbook/controller/GuestBookController.java
@@ -1,17 +1,66 @@
 package com.hanains.guestbook.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.hanains.guestbook.dao.GuestBookDAO;
+import com.hanains.guestbook.vo.GuestBookVo;
 
 @Controller
 public class GuestBookController {
+	
+	@Autowired 
+	private GuestBookDAO guestbookDao;
+	
+	
 	@RequestMapping("/")
-	public String index(){
+	public String index(Model model){
 		
+		List<GuestBookVo> list = guestbookDao.getList();
+		
+		model.addAttribute("list", list);
+		System.out.println("[info]complete get list");
 		return "/WEB-INF/views/index.jsp";
 		
 	}
 	
+	@RequestMapping(value="/deleteForm/{no}", method= RequestMethod.GET)
+	public String deleteForm(@PathVariable("no") Long no, Model model){
+		model.addAttribute("no", no);
+		return "/WEB-INF/views/deleteform.jsp";
+	}
+
+	@RequestMapping(value="/delete", method=RequestMethod.POST)
+	public String delete(@RequestParam(value = "no", required = true, defaultValue = "-1") Long no,
+						 @RequestParam(value = "password", required = true, defaultValue = "") String password)
+	{
+		
+		GuestBookVo vo = new GuestBookVo();
+		vo.setNo(no);
+		vo.setPassword(password);
+				
+		guestbookDao.delete(vo);
+		
+		return "redirect:/";
+	}
+	
+	@RequestMapping(value="/add", method=RequestMethod.POST)
+	public String add(@ModelAttribute GuestBookVo vo){
+		
+		guestbookDao.insert(vo);
+		
+		return "redirect:/";
+		
+	}
 	
 	
 }
--- webapp/WEB-INF/views/index.jsp
+++ webapp/WEB-INF/views/index.jsp
@@ -1,14 +1,60 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn"%>
-<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
-<!DOCTYPE html>
+
+<%
+	pageContext.setAttribute("crlf", "\r\n");
+%>
+
+<%--
+
+					 방명록 애플리케이션 (Spring)
+											정기남
+
+ --%>
+
 <html>
 <head>
-<meta charset="UTF-8">
-<title>Insert title here</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>방명록</title>
 </head>
 <body>
-hi
+	<form action="/guestbook3/add" method="post">
+		<table border=1 width=500>
+			<tr>
+				<td>이름</td>
+				<td><input type="text" name="name"></td>
+				<td>비밀번호</td>
+				<td><input type="password" name="password"></td>
+			</tr>
+			<tr>
+				<td colspan=4><textarea name="message" cols=60 rows=5></textarea></td>
+			</tr>
+			<tr>
+				<td colspan=4 align=right><input type="submit" VALUE=" 확인 "></td>
+			</tr>
+		</table>
+	</form>
+	<br>
+
+	<c:set var="count" value="${fn:length(list)}" />
+	<c:forEach items="${list}" var="vo" varStatus="status">
+		<table width=510 border=1>
+			<tr>
+				<td>${count-status.index}</td>
+				<td>${vo.name}</td>
+				<td>${vo.date}</td>
+				<td><a href="/guestbook3/deleteForm/${vo.no}">삭제</a></td>
+			</tr>
+			<tr>
+				<td colspan=4>${fn:replace(vo.message,crlf,'<br/>')}</td>
+			</tr>
+		</table>
+		<br>
+
+	</c:forEach>
+
 </body>
 </html>
