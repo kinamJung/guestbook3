a1f0be631d2764be3a24d4e1a7b9a7017ef47682
bit-user <bit-user@bit>
2015-12-11 14:34:10 +0900

    init

---
+++ src/main/java/com/hanains/guestbook/controller/GuestBookController.java
@@ -0,0 +1,17 @@
+package com.hanains.guestbook.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class GuestBookController {
+	@RequestMapping("/")
+	public String index(){
+		
+		return "/WEB-INF/views/index.jsp";
+		
+	}
+	
+	
+	
+}
